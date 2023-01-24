641e0727540c68184ac8d720621f25e420cd57e6
James <jjguevarag@gmail.com>
2023-01-24 11:58:32 -0600

    update for colors

--- scripts/form.js
+++ scripts/form.js
@@ -1,8 +1,11 @@
 function formDisplay() {
   var formulario = document.getElementById("form-content")
+  // @ts-ignore
   if (formulario.style.display === "none") {
+      // @ts-ignore
       formulario.style.display = "block";
   } else {
+      // @ts-ignore
       formulario.style.display = "none";
   }
 }
